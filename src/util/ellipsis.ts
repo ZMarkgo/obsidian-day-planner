0397fbcaa38e6d793159f95ddcaecdbe9353221b
zmark <351479188@qq.com>
2024-04-28 10:52:38 +0800

    perf: modify ellipsis function to handle link in status-bas-widget

--- src/util/ellipsis.ts
+++ src/util/ellipsis.ts
@@ -1,6 +1,10 @@
 export function ellipsis(input: string, limit: number) {
-  if (input.length <= limit) {
-    return input;
+  // for [[t1|t2]] or [[t2]], replace with t2 ;
+  const text = input.replace(/\[\[(.*?)(?:\|(.*?)|)\]\]/g, (match, t1, t2) =>
+    t2 ? t2 : t1,
+  );
+  if (text.length <= limit) {
+    return text;
   }
-  return input.substring(0, limit) + "...";
+  return text.substring(0, limit) + "...";
 }
